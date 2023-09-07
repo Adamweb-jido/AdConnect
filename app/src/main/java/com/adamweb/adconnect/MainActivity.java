32f79af3635bf5f27376a277076deeef41b957a2
Real_Adamweb <117515903+Adamweb-jido@users.noreply.github.com>
2023-09-07 12:45:52 +0100

    my Wallet For Data And Other Subscriptions

--- app/src/main/java/com/adamweb/adconnect/MainActivity.java
+++ app/src/main/java/com/adamweb/adconnect/MainActivity.java
@@ -2,13 +2,23 @@ package com.adamweb.adconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+  TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
